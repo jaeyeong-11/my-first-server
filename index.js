6e940ab67dfebf3fc696a8b3225c2d84757e9de3
CHOI JAEYOUNG <yj2510625@yangjae.sen.hs.kr>
2026-02-20 16:03:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path'); // 1. path 모듈 불러오기 (기본 내장이라 설치 필요 없음)
 const app = express();
 
-app.use(express.static('public'));
+// 2. 더 강력한 방법으로 public 폴더 연결하기
+// __dirname은 현재 파일이 있는 폴더의 절대 주소를 뜻해!
+app.use(express.static(path.join(__dirname, 'public')));
 
-// 이 부분은 이제 index.html이 대신하니까 지워도 돼!
-// app.get('/', (req, res) => { ... }); 
+// 만약 위 코드로도 안 된다면, 강제로 index.html을 보내주는 코드를 추가해 보자
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -12,3 +17,4 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
